3a857d580881fac227c928d811a687d4a30a15ca
soeaver Yang <1013608889@qq.com>
2017-07-11 16:20:25 +0800

    Update painter.py

--- painter.py
+++ painter.py
@@ -12,15 +12,18 @@ val_acc = []
 for i in f:
     if i.strip().split('\t')[0] == 'Learning Rate':
         continue
-    train_acc.append(float(i.strip().split('\t')[3]))
-    val_acc.append(float(i.strip().split('\t')[4]))
+    train_acc.append(100 - float(i.strip().split('\t')[3]))
+    val_acc.append(100 - float(i.strip().split('\t')[4]))
 
-# print train_acc, val_acc
+plt.ylim(0, 100)
+plt.xlabel('epochs')
+plt.ylabel('top-1 error(%)')
 
+# plt.plot(train_acc, linestyle='-.')
 plt.plot(train_acc)
 plt.plot(val_acc)
 
-plt.legend(['{}-train-acc'.format(model_name), '{}-val-acc'.format(model_name)])
+plt.legend(['{}-train-error'.format(model_name), '{}-val-error'.format(model_name)])
 plt.grid(True)
 
 plt.savefig('{}-log.png'.format(model_name), dpi=150)
