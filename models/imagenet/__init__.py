81589c6a6db7a106d701f48f8a14e8676b0270de
Yang <1013608889@qq.com>
2017-08-11 14:00:59 +0800

    Update __init__.py

--- models/imagenet/__init__.py
+++ models/imagenet/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 
 from .resnext import *
-from .aligned_inception_resnet import *
-from .aligned_inception_resnext import *
 from .bigass import *
 from .se_resnext import *
 from .se_air import *
